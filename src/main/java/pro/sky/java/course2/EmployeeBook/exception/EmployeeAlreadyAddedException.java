892f65ae569da0a6c4311177b70370e07405e498
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-05-17 00:37:23 +0300

    exceptions and validInput

--- src/main/java/pro/sky/java/course2/EmployeeBook/exception/EmployeeAlreadyAddedException.java
+++ src/main/java/pro/sky/java/course2/EmployeeBook/exception/EmployeeAlreadyAddedException.java
@@ -1,8 +1,6 @@
 package pro.sky.java.course2.EmployeeBook.exception;
 
 public class EmployeeAlreadyAddedException extends RuntimeException {
-    public EmployeeAlreadyAddedException() {
-    }
 
     public EmployeeAlreadyAddedException(String message) {
         super(message);
---
+++ src/test/java/pro/sky/java/course2/EmployeeBook/service/EmployeeBookServiceImplTest.java
@@ -0,0 +1,39 @@
+package pro.sky.java.course2.EmployeeBook.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pro.sky.java.course2.EmployeeBook.exception.EmployeeAlreadyAddedException;
+import pro.sky.java.course2.EmployeeBook.exception.EmployeeNotFoundException;
+import pro.sky.java.course2.EmployeeBook.model.Employee;
+
+public class EmployeeBookServiceImplTest {
+
+    private final EmployeeBookServiceImpl out = new EmployeeBookServiceImpl();
+
+
+    @Test
+    public void shouldWellWorkWhenValidInput() {
+        Employee result = out.add("Abc", "Abc", "Abc");
+        Assertions.assertEquals(out.find("Abc", "Abc", "Abc"), result);
+        Assertions.assertEquals(out.remove("Abc", "Abc", "Abc"), result);
+    }
+
+    @Test
+    public void shouldThrowEmployeeNotFoundExceptionWhenFind() {
+        Assertions.assertThrows(EmployeeNotFoundException.class,
+                () -> out.find("Abc", "Abc", "Abc"));
+    }
+
+    @Test
+    public void shouldThrowEmployeeNotFoundExceptionWhenRemove() {
+        Assertions.assertThrows(EmployeeNotFoundException.class,
+                () -> out.remove("Abc", "Abc", "Abc"));
+    }
+
+    @Test
+    public void shouldThrowEmployeeAlreadyAddedExceptionWhenReAdd() {
+        Employee result = out.add("Abc", "Abc", "Abc");
+        Assertions.assertThrows(EmployeeAlreadyAddedException.class,
+                () -> out.add("Abc", "Abc", "Abc"));
+    }
+}
